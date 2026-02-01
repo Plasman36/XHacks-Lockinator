cb4d6e13abc4b511ad92ecf810f1d9eab2a6a731
Joyce Abraham <jaa146@sfu.ca>
2026-02-01 02:22:00 -0800

    tiemr

--- tab tracking/popup.js
+++ tab tracking/popup.js
@@ -1,28 +1,45 @@
 console.log("This is a popup!")
 
+const BLACKLIST = ["instagram.com"];
 let timeLeft = 3;
+
 const timerEl = document.getElementById("timer");
-
-const countdown = setInterval(() => {
-    timeLeft--;
-
-    if (timeLeft > 0) {
-        timerEl.textContent = `Slap in ${timeLeft}...`;
-    }else {
-        timerEl.textContent = "Get Slapped";
-        clearInterval(countdown);
-
-        window.close();
-    }
-}, 1000);
+const list = document.getElementById("tabs");
 
 chrome.storage.local.get("openTabs", (data) => {
     const tabs = data.openTabs || {};
-    const list = document.getElementById("tabs");
+    let hasBadTab = false;
 
     for (const id in tabs) {
-        const li = document.createElement("li");
-        li.textContent = `${tabs[id].title} (${tabs[id].url})`;
-        list.appendChild(li);
+        const { title, url } = tabs[id];
+
+        if (url && BLACKLIST.some(domain => url.includes(domain))) {
+            hasBadTab = true;
+
+            const li = document.createElement("li");
+            li.textContent = `${title} (${url})`;
+            list.appendChild(li);
+        }
     }
+
+    if (!hasBadTab) {
+        window.close();
+        return;
+    }
+    startCountdown();
 });
+
+function startCountdown() {
+    timerEl.textContent = `Slap in ${timeLeft}...`;
+    const countdown = setInterval(() => {
+        timeLeft--;
+
+        if (timeLeft > 0) {
+            timerEl.textContent = `Slap in ${timeLeft}...`;
+        } else {
+            clearInterval(countdown);
+            timerEl.textContent = "Get Slapped";
+            window.close();
+        }
+    }, 1000);
+}
